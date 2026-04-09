837c4876d72eb9c71ddf6cd44672a0f2f9f9f290
devano2068 <devano27921@gmail.com>
2026-04-09 18:45:19 +0700

    frist commit

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -1,41 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Data.SqlClient;
-
-namespace CRUDMahasiswaADO
-{
-    public partial class FormMahasiswa : Form
-    {
-        private readonly string connectionString =
-            "Data Source=LAPTOP-3UJU5DJS\\VANTAMERAH;Initial Catalog=DBAkademikADO;Integrated Security=True";
-
-        public FormMahasiswa()
-        {
-            InitializeComponent();
-        }
-
-        private void buttonConnect_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    MessageBox.Show("Koneksi berhasil");
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Koneksi gagal: " + ex.Message);
-            }
-        }
+﻿
 
         private void btnLoad_Click(object sender, EventArgs e)
         {
